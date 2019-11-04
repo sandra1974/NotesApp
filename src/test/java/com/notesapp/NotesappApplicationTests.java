74693476b01c12fa7cccaf6a4ca741ccb3c59be7
Sandra Dujmović <sandra@netcom.local>
2019-11-04 14:40:26 +0100

    first commit

---
+++ src/test/java/com/notesapp/NotesappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.notesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class NotesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
