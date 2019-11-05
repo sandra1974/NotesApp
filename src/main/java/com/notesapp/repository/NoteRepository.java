b7c19d8d0366fb1635af5ccb9a1e10d0b70ac28f
Sandra <32596579+sandra1974@users.noreply.github.com>
2019-11-05 10:52:24 +0100

    Delete NoteRepository.java

--- src/main/java/com/notesapp/repository/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.notesapp.repository;
-import com.notesapp.model.Note;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends MongoRepository<Note, String> {
-}
